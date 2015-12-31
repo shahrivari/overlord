5fd1749df837d3c5c040ed00d92bf9f6027be246
Saeed Shahrivari <saeed.shahrivari@gmail.com>
2015-12-31 22:39:35 +0330

    initial commit.

--- src/main/java/amu/saeed/overlord/type/KeyValue.java
+++ src/main/java/amu/saeed/overlord/type/KeyValue.java
@@ -1,24 +1,30 @@
 package amu.saeed.overlord.type;
 
+import org.msgpack.core.MessagePack;
+import org.msgpack.core.MessagePacker;
+import org.msgpack.core.MessageUnpacker;
+
 import java.io.*;
 import java.util.Base64;
 
 public class KeyValue implements Serializable, Externalizable {
-    private byte[] key;
+    public static final byte VERSION = 1;
+    private static final Base64.Encoder ENCODER_64 = Base64.getEncoder();
+    private long key;
     private byte[] value;
-    private Base64.Encoder encoder64 = Base64.getEncoder();
-
     public KeyValue() {
-        key = new byte[0];
+        key = 0L;
         value = new byte[0];
     }
 
-    public KeyValue(byte[] key, byte[] value) {
+
+
+    public KeyValue(long key, byte[] value) {
         this.key = key;
         this.value = value;
     }
 
-    public byte[] getKey() {
+    public long getKey() {
         return key;
     }
 
@@ -28,24 +34,62 @@ public class KeyValue implements Serializable, Externalizable {
 
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
-        out.writeInt(key.length);
+        out.write(VERSION);
+        out.writeLong(value.length);
         out.writeInt(value.length);
-        out.write(key);
         out.write(value);
     }
 
     @Override
     public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-        key = new byte[in.readInt()];
-        value = new byte[in.readInt()];
-        in.read(key);
-        in.read(value);
+        byte version = in.readByte();
+        if (version == 1) {
+            key = in.readLong();
+            value = new byte[in.readInt()];
+            in.read(value);
+        }
+        throw new IllegalStateException("Bad version: " + version);
     }
 
     public String toJson() {
-        StringBuilder builder = new StringBuilder((key.length + value.length) * 4 / 3);
-        builder.append("{\n\"key\":\"").append(encoder64.encodeToString(key)).append("\",\n\"value\":\"").append
-            (encoder64.encodeToString(value)).append("\"\n}");
+        StringBuilder builder = new StringBuilder((value.length + 10) * 4 / 3);
+        builder.append("{\n\"key\":\"").append(key).append("\",\n\"value\":\"")
+                .append(ENCODER_64.encodeToString(value)).append("\"\n}");
         return builder.toString();
+    }
+
+    public ByteArrayOutputStream toMessagePackStream() {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        MessagePacker packer = MessagePack.newDefaultPacker(stream);
+        try {
+            packer.packByte(VERSION);
+            packer.packLong(key);
+            packer.packInt(value.length);
+            packer.writePayload(value);
+            packer.close();
+            return stream;
+        } catch (IOException e) {
+            throw new IllegalStateException("Error while packing.", e);
+        }
+    }
+
+    public byte[] toMessagePackArray() {
+        return toMessagePackStream().toByteArray();
+    }
+
+    public KeyValue fromMessagePack(byte[] bytes) {
+        try {
+            MessageUnpacker unpacker = MessagePack.newDefaultUnpacker(bytes);
+            byte version = unpacker.unpackByte();
+            if (version == 1) {
+                key = unpacker.unpackLong();
+                value = new byte[unpacker.unpackInt()];
+                unpacker.readPayload(value);
+                return this;
+            }
+            throw new IllegalStateException("Bad version: " + version);
+        } catch (IOException e) {
+            throw new IllegalStateException("Error while unpacking.", e);
+        }
     }
 }
